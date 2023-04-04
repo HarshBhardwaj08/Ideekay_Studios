fcb776c199388e625f79c7cf8beec7345063e39e
HarshBhardwaj-2001 <harshbhardwaj882536@gmail.com>
2023-04-04 20:31:41 +0530

    Traps

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rg2d;
+    [SerializeField] float speed;
+    void Start()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rg2d.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rg2d.velocity.y);
+    }
+}
