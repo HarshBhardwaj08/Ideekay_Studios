a443c043ca3d17736232fcaebda1696cbd25929c
HarshBhardwaj-2001 <harshbhardwaj882536@gmail.com>
2023-04-05 00:50:19 +0530

    Fall_Spike

---
+++ Assets/Scripts/FallSpike.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallSpike : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rg2d;
+    void Start()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > 3) {
+            rg2d.gravityScale = 1;
+        }
+    }
+}
